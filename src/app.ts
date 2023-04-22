f5a41ce275e9053781740859ed8618db9acd2b50
Tony <tonygovernmentemail@gmail.com>
2023-04-22 11:30:48 -0600

    Initial void

---
+++ src/app.ts
@@ -0,0 +1,67 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { createScene } from "./scene";
+
+function constructCanvas(): HTMLCanvasElement{
+    var canvas = document.createElement("canvas");
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.id = "gameCanvas";
+    document.body.appendChild(canvas);
+    return canvas
+}
+
+export const canvas = constructCanvas();
+export const engine = new Engine(canvas, true);
+
+async function main():Promise<void>{
+    var scene = await createScene();
+    
+
+    // run the main render loop
+    engine.runRenderLoop(() => {
+        scene.render();
+    });
+
+    
+}
+
+class App {
+    constructor() {
+        // create the canvas html element and attach it to the webpage
+        var canvas = constructCanvas()
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i') {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                } else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+
+        // run the main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+
+        const xr = scene.createDefaultXRExperienceAsync();
+        
+    }
+}
+// new App();
+main();
---
+++ src/scene.ts
@@ -0,0 +1,81 @@
+import { Scene } from "@babylonjs/core/scene";
+import { canvas, engine } from "./app";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Mesh, MeshBuilder } from "@babylonjs/core/Meshes";
+import { Color3, Color4, HDRCubeTexture, PBRMaterial, PointLight, SceneLoader, UniversalCamera } from "@babylonjs/core";
+import { voidMaterial } from "./void_material";
+
+
+export async function createScene(): Promise<Scene>{
+    var scene = new Scene(engine);
+    var hdrTexture = new HDRCubeTexture("resources/full_hdr_dark.hdr",scene,512);
+    scene.clearColor = new Color4(0,0,0,1);
+    scene.ambientColor = new Color3(0.3,0.3,0.3);
+    scene.createDefaultSkybox(hdrTexture,true, 100000);
+    scene.createDefaultEnvironment({
+        environmentTexture: hdrTexture,
+        skyboxColor: new Color3(0,0,0),
+    })
+    
+    // var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+    var camera : UniversalCamera = new UniversalCamera("Camera", new Vector3(0,100,-4000), scene);
+    camera.maxZ=0;
+    camera.attachControl(canvas, true);
+    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    // var pointlight: PointLight = new PointLight("pointlight", new Vector3(0,-3,0), scene);
+    var pointlight: PointLight = new PointLight("pointlight", new Vector3(0,-30,0), scene);
+    // pointlight.intensity = 1000;
+    pointlight.intensity = 1000000;
+
+    var thevoid = await SceneLoader.ImportMeshAsync("Circle", "resources/", "void.glb");
+    thevoid.meshes[1].material = voidMaterial();
+
+    // var pbr = new PBRMaterial("pbr", scene);
+    // pbr.albedoColor = Color3.FromHSV(0,0,0).toLinearSpace();
+    // pbr.metallic = 0.8;
+    // pbr.roughness = 0.05;    
+    // pbr.subSurface.refractionIntensity = 0.8;
+    // pbr.subSurface.isRefractionEnabled = true;
+    // pbr.subSurface.indexOfRefraction = 1.1;
+    // pbr.subSurface.translucencyIntensity = 0.1;
+    // pbr.subSurface.isTranslucencyEnabled = true;
+    // pbr.clearCoat.isEnabled = true;
+    // pbr.sheen.isEnabled = true;
+    
+    // thevoid.meshes[1].material = pbr;
+
+    thevoid.meshes[1].scaling = new Vector3(1000,1000,1000);
+    
+    
+
+
+    window.addEventListener("keydown", (ev) => {
+        // Shift+Ctrl+Alt+I
+        if (ev.shiftKey && ev.ctrlKey) {
+            if (scene.debugLayer.isVisible()) {
+                scene.debugLayer.hide();
+            } else {
+                scene.debugLayer.show();
+            }
+        }
+    });
+    
+    
+    const xr = await scene.createDefaultXRExperienceAsync();
+
+    scene.registerBeforeRender(()=>{
+        update(scene);
+        xr.baseExperience.camera.position.setAll(3);
+    });
+    scene.registerAfterRender(()=>{
+    });
+    
+    return scene;
+}
+
+// scene.deltaTime for frame delta
+function update(scene: Scene): void{
+    
+}
